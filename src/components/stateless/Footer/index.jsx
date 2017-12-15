80e58b9000f8bdb85ad663d53f33051d71c07976
Marco Perez <gabriel.rodriguez@domandtom.com>
2017-12-15 00:24:04 -0600

    todo App with React and Redux

---
+++ src/components/stateless/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import FilterLink from '../../statefull/FilterLink';
+
+const Footer = () => {
+  return (
+    <p>
+      Show:
+      {' '}
+      <FilterLink filter="SHOW_ALL">All</FilterLink>
+      {', '}
+      <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>
+      {', '}
+      <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>
+    </p>
+  )
+}
+
+export default Footer
---
+++ src/components/stateless/Link/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Link = (props) => {
+  if (props.active) {
+    return <span>{props.children}</span>
+  }
+
+  return (
+    <a 
+      href=""
+      onClick={ (e) => { e.preventDefault(); props.onClick(); } }
+    >
+    {props.children}
+    </a>
+  )
+
+}
+
+export default Link;
